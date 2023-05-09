0ca3f898faad9f209b214a222f0c7184096aaf6c
krisnasirait <krisnasirait48@gmail.com>
2023-05-09 18:02:09 +0700

    update on ui

--- index.php
+++ index.php
@@ -1,23 +1,56 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Daftar Kontak</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar Kontak</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+        th, td {
+            padding: 8px;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+        tr:hover {
+            background-color: #f5f5f5;
+        }
+    </style>
 </head>
 <body>
-	<h1>Daftar Kontak</h1>
-	<ul>
-	<?php
-		// Mendapatkan data dari backend
-		$url = getenv('BACKEND_URL') . '/kontak';
-		$data = file_get_contents($url);
-		$kontak = json_decode($data);
-		
-		// Menampilkan data ke halaman
-		foreach ($kontak as $k) {
-			echo '<li>' . $k->nama . ' (' . $k->telepon . ')</li>';
-		}
-	?>
-	</ul>
+    <h1>Daftar Kontak</h1>
+    <?php
+    $contacts = [
+        ['name' => 'Budi', 'phone' => '08123456789', 'email' => 'budi@example.com'],
+        ['name' => 'Ani', 'phone' => '08129876543', 'email' => 'ani@example.com'],
+        ['name' => 'Siti', 'phone' => '08512398765', 'email' => 'siti@example.com'],
+    ];
+    ?>
+    <table>
+        <thead>
+            <tr>
+                <th>Nama</th>
+                <th>Telepon</th>
+                <th>Email</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($contacts as $contact): ?>
+                <tr>
+                    <td><?php echo htmlspecialchars($contact['name']); ?></td>
+                    <td><?php echo htmlspecialchars($contact['phone']); ?></td>
+                    <td><?php echo htmlspecialchars($contact['email']); ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
 </body>
 </html>
